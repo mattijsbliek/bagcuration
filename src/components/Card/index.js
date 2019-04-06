e103be9d691d40362165118aaeeb28617b2f38f1
Mattijs Bliek <mattijs@mollie.com>
2019-04-06 21:33:31 +0200

    Crop front-page images.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,7 +6,11 @@ import styles from './styles.module.css';
 const Card = ({ name, subheading, to, image }) => (
   <Link className={styles.card} to={to}>
     <figure className={styles.figure}>
-      <img src={withPrefix(image)} className={styles.image} alt="" />
+      <img
+        src={withPrefix(`${image}?nf_resize=smartcrop&w=390&h=490`)}
+        className={styles.image}
+        alt=""
+      />
     </figure>
     <h2 className={styles.title}>{name}</h2>
     <p className={styles.subheading}>{subheading}</p>
